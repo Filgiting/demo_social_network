35ffec5d56233b7326f1145e11d3923bd51689d7
Filgiting <lextermenf@gmail.com>
2020-05-18 18:11:17 +0600

    Updates

---
+++ precache-manifest.376a9334e065b61ef3c6a16f9164c1fb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800a77d9ebdb0227a71af26bcf31e790",
+    "url": "/demo_social_network/index.html"
+  },
+  {
+    "revision": "250a5e107ac7ddeb8ef3",
+    "url": "/demo_social_network/static/css/3.7cbf4148.chunk.css"
+  },
+  {
+    "revision": "b684062ecd5d7584cb8a",
+    "url": "/demo_social_network/static/css/4.0dac86de.chunk.css"
+  },
+  {
+    "revision": "d6c4b3092fe63a53aa61",
+    "url": "/demo_social_network/static/css/main.28b7630a.chunk.css"
+  },
+  {
+    "revision": "eeeb1070d964279d0b97",
+    "url": "/demo_social_network/static/js/2.55e6d073.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/demo_social_network/static/js/2.55e6d073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250a5e107ac7ddeb8ef3",
+    "url": "/demo_social_network/static/js/3.c1067fb4.chunk.js"
+  },
+  {
+    "revision": "b684062ecd5d7584cb8a",
+    "url": "/demo_social_network/static/js/4.2de2c91a.chunk.js"
+  },
+  {
+    "revision": "d6c4b3092fe63a53aa61",
+    "url": "/demo_social_network/static/js/main.2f6ed750.chunk.js"
+  },
+  {
+    "revision": "b2699e6a161034c2fd4b",
+    "url": "/demo_social_network/static/js/runtime-main.70bdff9d.js"
+  },
+  {
+    "revision": "d89580c222d7e2e7b13905a9aa4ba247",
+    "url": "/demo_social_network/static/media/User.d89580c2.png"
+  },
+  {
+    "revision": "83d3f2cb88b143b30885fe0b5d0f0dfe",
+    "url": "/demo_social_network/static/media/preloader.83d3f2cb.svg"
+  }
+]);
