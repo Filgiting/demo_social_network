f47b8e5bbbf9c98123db6a602c8a37cf17027fc2
Filgiting <lextermenf@gmail.com>
2020-05-18 21:20:16 +0600

    Completed application

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import store from './redux/redux-store';
 import {Provider} from 'react-redux';
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
